f2ffd3cfb2be55f901001a03f2c146dce45ea0ad
Lacy Morrow <me@lacymorrow.com>
2025-04-18 15:16:05 -0400

    auto commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import FloatingBar from "./FloatingBar";
 import "./styles/globals.css";
-// import "./styles/App.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
