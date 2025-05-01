d5aea727272baaac89bb13898f6e9d3dc7d74440
Lacy Morrow <me@lacymorrow.com>
2025-05-01 00:10:35 -0400

    auto commit

---
+++ src/commands/element.rs
@@ -0,0 +1,4 @@
+use juno_lib::models::{Element, RequestData, ResponseData};
+use juno_lib::platform::PlatformElementTrait;
+use juno_lib::state::AppState;
+// ... existing code ...
---
+++ src/commands/keyboard.rs
@@ -0,0 +1,4 @@
+use juno_lib::models::{RequestData, ResponseData};
+use juno_lib::platform::PlatformKeyboardTrait;
+use juno_lib::state::AppState;
+// ... existing code ...
