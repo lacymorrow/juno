b3d6ee2cfc150717278a2072eb298dac1c1d1ab4
Lacy Morrow <me@lacymorrow.com>
2025-06-17 23:28:04 -0400

    auto commit

--- src/main.tsx
+++ src/main.tsx
@@ -9,8 +9,8 @@ import { VoiceProvider } from "./contexts/VoiceContext";
 import FloatingPanel from "./FloatingPanel";
 import OnboardingWindow from "./OnboardingWindow";
 import "./styles/globals.css";
-import SettingsWindow from "./components/OLD-SettingsWindow";
-import Settings from "./components/OLD-Settings";
+// import SettingsWindow from "./components/OLD-SettingsWindow";
+// import Settings from "./components/OLD-Settings";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
